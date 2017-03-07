845ebe056c3931e6a8b65d11fbc973ec723961f4
Ashwini <ashie.oh@gmail.com>
2017-03-07 17:17:47 +0000

    added a get route to show form and a post route to add link to database

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -3,13 +3,23 @@ require './app/models/link'
 
 class BookmarkManager < Sinatra::Base
 
+
   get '/' do
-    "Hello World"
+    "Hello World" # infrastructure set-up check
   end
 
   get '/links' do
     @links = Link.all
     erb :'links/index'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect '/links'
   end
 
   run if app_file == $0
