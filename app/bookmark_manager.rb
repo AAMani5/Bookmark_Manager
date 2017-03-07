36203dc8c07f7440e98c533aea1d153c4250ebae
Ashwini <ashie.oh@gmail.com>
2017-03-07 16:25:28 +0000

    moved inside app folder

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -1,9 +1,15 @@
 require 'sinatra/base'
+require './app/models/link'
 
 class BookmarkManager < Sinatra::Base
 
   get '/' do
     "Hello World"
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
   end
 
   run if app_file == $0
