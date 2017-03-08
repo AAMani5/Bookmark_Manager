3a5118e98381f5867a44fe38c702a119dd0e79db
Ashwini <ashie.oh@gmail.com>
2017-03-08 10:27:59 +0000

    Adds a new tag field to form

---
+++ spec/features/tagging_link_spec.rb
@@ -0,0 +1,14 @@
+feature "Creating new links" do
+
+  scenario "be able to add new links" do
+    visit('/links/new')
+    fill_in :title, with: 'Google'
+    fill_in :url, with: 'http://www.google.co.uk'
+    fill_in :tags, with: 'search'
+    click_button 'Add Link'
+    within 'ul#links' do # need to have this inside the unordered list not in general page
+      expect(page).to have_content('Google')
+    end
+  end
+
+end
