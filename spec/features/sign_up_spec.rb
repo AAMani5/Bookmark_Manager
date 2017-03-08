c3b16f479cbf31822143205b0feee5148e10b1d8
Ashwini <ashie.oh@gmail.com>
2017-03-08 19:09:49 +0000

    Adds a feature spec for signing up with email and pwd

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,12 @@
+feature "Sign Up for services" do
+
+  scenario "be able to sign up in my account" do
+    visit('/signup')
+    fill_in :email, with: 'test@test.com'
+    fill_in :password, with: '1234'
+    expect{click_button 'Sign Up'}.to change{User.count}.by 1
+    expect(page).to have_current_path('/links')
+    expect(page).to have_content('Hello test@test.com, Welcone to BookmarkManager')
+  end
+
+end
