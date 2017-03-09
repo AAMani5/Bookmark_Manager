1f967e96a774fdd405f543bb27a48eb03cb4c573
Ashwini <ashie.oh@gmail.com>
2017-03-09 18:29:59 +0000

    Extends to include 'flash' message feature

--- spec/features/password_confirmation_spec.rb
+++ spec/features/password_confirmation_spec.rb
@@ -6,7 +6,8 @@ feature "Password confirmation" do
     fill_in :password, with: 'firstone'
     fill_in :password_confirmation, with: 'secondone'
     expect{click_button 'Sign Up'}.to_not change(User, :count)
-    expect(page).to have_current_path('/links')
+    expect(page).to have_current_path('/users')
+    expect(page).to have_content 'Password and confirmation password do not match'
   end
 
 end
