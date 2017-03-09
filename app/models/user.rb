8a6d77cfc63cf60d8488cc9a34b436509cd20808
Ashwini <ashie.oh@gmail.com>
2017-03-09 16:38:25 +0000

    Adds an additional arg password_confirmation params to User.create method
    1. User model has 2 new instance variables : @password and @password_confirmation
    2. User.create in bookmark_manager.rb now includes addition arg(password_confirmation)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,18 @@ class User
   property :email,      String
   property :password_salt,   Text
 
+  attr_accessor :password_confirmation # write access for User.create method in bookmark_manager.rb, read access for validates_confirmation_of method
+  attr_reader :password # read access to validates_confirmation_of method
+
   # password will hold password & salt
   # password property is text not String coz pwd + salt will be > 50 chars
   # has n, :links
 
   def password=(password)
+    @password = password
     self.password_salt = BCrypt::Password.create(password)
   end
+
+  validates_confirmation_of :password  # model will save and be valid only if password == password_confirmation
 
 end
