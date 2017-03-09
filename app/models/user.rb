320729e4edf3b49e53c11828d78e3c01d232be90
Ashwini <ashie.oh@gmail.com>
2017-03-09 15:15:53 +0000

    Adds a password setter method, which uses bcrypt to add salt & returns hashed pwd

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,14 @@ class User
 
   property :id,         Serial
   property :email,      String
-  property :password,   String
+  property :password_salt,   Text
 
+  # password will hold password & salt
+  # password property is text not String coz pwd + salt will be > 50 chars
   # has n, :links
+
+  def password=(password)
+    self.password_salt = BCrypt::Password.create(password)
+  end
 
 end
