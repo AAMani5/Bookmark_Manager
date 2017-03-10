dffb635a400b4ad21e607f4562dfc7989a81bdb0
Ashwini <ashie.oh@gmail.com>
2017-03-10 15:56:26 +0000

    added uniqueness validation & customised error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User
   include DataMapper::Resource
 
   property :id,         Serial
-  property :email,      String, :required => true, :format => :email_address
+  property :email,      String, :required => true, :format => :email_address, :unique => true,
+  :messages => {
+     :presence  => "We need your email address.",
+     :is_unique => "We already have that email.",
+     :format    => "Doesn't look like an email address to me ..."
+   }
   property :password_salt,   Text
 
   attr_accessor :password_confirmation # write access for User.create method in bookmark_manager.rb, read access for validates_confirmation_of method
@@ -20,6 +25,6 @@ class User
   end
 
   # validates_format_of :email, as: :email_address # :format => :email_address with property creates this line implicitly
-  validates_confirmation_of :password  # model will save and be valid only if password == password_confirmation
+  validates_confirmation_of :password, :message => "Password and confirmation password do not match" # model will save and be valid only if password == password_confirmation
   # validates_presence_of :email # can also say :required => true when property decalred
 end
